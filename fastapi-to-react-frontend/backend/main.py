b4291b0bc83a20e7a2eddf038cec013b1df3c938
Aaron Dantley <aarondantle@gmail.com>
2025-04-29 14:50:13 -0400

    feat: adding basic Fruit API

--- fastapi-to-react-frontend/backend/main.py
+++ fastapi-to-react-frontend/backend/main.py
@@ -4,5 +4,59 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 
-#
-class 
+"""
+Model Setup
+"""
+class Fruit(BaseModel):
+    name: str
+
+class Fruits(BaseModel):
+    fruits: List[Fruit]
+
+app = FastAPI()
+
+"""
+ CORS Configuration: 
+ 
+ defines base origin of any domain that needs to be accessed; this is a list of trusted sources, and block out requests from any other domain
+
+"""
+origins = [
+    "http://localhost:3000",  # React app port
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+"""
+Mocking DB with Dictionary
+"""
+memory_db = {"fruits": []}
+
+"""
+API Endpoints
+"""
+
+@app.get(path="/fruits", response_model=Fruits)
+def get_fruits():
+    """
+    Get all fruits from the mock database
+    """
+    return Fruits(fruits=memory_db["fruits"]) # automatically converts the list of fruits to the Fruits model and into JSON format
+
+
+@app.post(path="/fruits", response_model=Fruits)
+def add_fruit(fruit: Fruit):
+    """
+    Add a new fruit to the mock database
+    """
+    memory_db["fruits"].append(fruit)
+    return fruit # returns the newly added fruit in JSON format
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # Run the FastAPI app on port 8000
